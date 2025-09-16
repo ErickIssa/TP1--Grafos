733f892e0b6f2ebf408917e5fd48bf7429b5e022
ErickIssa <erickvinicius19675@gmail.com>
2025-09-15 21:15:30 -0300

    leitura de arquivo sem por no grafo ainda.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from src.menorcaminho import * #so pra testar como importa
+
+def ler_entrada():
+
+    arq = input("Digite o nome do arquivo de entrada, juntamente com sua extensão que está localizado na pasta data:\n") 
+    with open("data/" + arq, "r") as arquivo: #esse "r" e de read pra apenas ler a entrada
+        for linha in arquivo:
+            origem, destino, peso = linha.split(",")
+            peso = int(peso);
+            print(f"origem: {origem} destino: {destino} peso: {peso}")
+
+try:
+    ler_entrada();
+
+except FileNotFoundError:
+    print(f"Arquivo nao encontrado")
+
+
+
