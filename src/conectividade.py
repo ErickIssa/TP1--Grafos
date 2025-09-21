e9131c2abcfb6d14735cf06665553b45a2f2dedc
Júlio César <julioitabira@hotmail.com>
2025-09-21 15:29:43 -0300

    estrutura Grafo implementada. Articulacao e conectividade modularizado

---
+++ src/conectividade.py
@@ -0,0 +1,22 @@
+from grafo import Grafo
+
+class Conectividade (Grafo):
+    def __init__(self, vertices):
+        super().__init__(vertices)
+
+    def verificaConectividade(grafo, start=0):
+        visitados = [False] * grafo.V
+        pai = [None] * grafo.V
+        vertices = []
+
+        def dfs(u):
+            visitados[u] = True
+            for v in grafo.grafo[u]:
+                if not visitados[v]:
+                    pai[v] = u
+                    vertices.append((u, v))
+                    dfs(v)
+        dfs(start)
+        conectados = all(visitados)
+        return vertices, pai, conectados
+    
---
+++ src/grafo.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+class Grafo:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.grafo = defaultdict(list)  # lista de adjacência
+        self.time = 0                   # contador de tempo DFS (feito pelo algoritmo de referência)
+        self.articulaçoes = set()
+
+
+    def adiciona_aresta(self, u, v):
+        self.grafo[u].append(v)
+        self.grafo[v].append(u)
+
+    def dfs(self, u, visitados, pai, profundidade, low):
+            filhos = 0
+            visitados[u] = True
+            self.time += 1
+            profundidade[u] = low[u] = self.time
+
+            for v in self.grafo[u]:
+                if not visitados[v]:
+                    pai[v] = u
+                    filhos += 1
+                    self.dfs(v, visitados, pai, profundidade, low)
+
+                    # Atualiza o low[u] considerando o filho
+                    low[u] = min(low[u], low[v])
+
+                    # Condições para ponto de articulação
+                    if pai[u] is None and filhos > 1:
+                        self.articulaçoes.add(u)
+                    if pai[u] is not None and low[v] >= profundidade[u]:
+                        self.articulaçoes.add(u)
+
+                elif v != pai[u]:
+                    # Atualiza o low[u] com o pai alcançável
+                    low[u] = min(low[u], profundidade[v])
+
+
