5a5b16eed0cbdca970e34fa7314953ee1be56e99
Gabriel <gabrielamorim.pessoal@gmail.com>
2025-10-01 22:21:21 -0300

    fix: refazendo commits anteriores evitando merge

--- src/grafo.py
+++ src/grafo.py
@@ -1,3 +1,4 @@
+from src.menorCaminho import *
 from collections import defaultdict
 
 class Grafo:
@@ -8,9 +9,9 @@ class Grafo:
         self.articulaçoes = set()
 
 
-    def adiciona_aresta(self, u, v, peso = 1):
-        self.grafo[u].append((v, peso))
-        self.grafo[v].append((u, peso))
+    def adiciona_aresta(self, u, v,peso=1):
+        self.grafo[u].append((v,peso))
+        self.grafo[v].append((u,peso))
 
     def numeroDeCidades(self):
         return self.V
@@ -39,6 +40,29 @@ class Grafo:
                 elif v != pai[u]:
                     # Atualiza o low[u] com o pai alcançável
                     low[u] = min(low[u], profundidade[v])
+
+    def quantidadeVizinhos(self, cidade):
+        if isinstance(cidade, str): #serve pra verificar se e realmente uma letra que ele tá recebendo
+            cidade = converter(cidade)
+
+        return len(self.grafo[cidade])
+
+    def converter(x):
+        if isinstance(x, str):
+            return ord(x.upper()) - 65
+        return chr(x + 65)
+
+    def defineQntdVertices(arq):
+        vetorLetras = set() #permite inserir uma vez em so
+        
+        with open("data/" + arq, "r") as arquivo:
+            for linha in arquivo:
+                origem, destino, peso = linha.strip().split(",")
+                vetorLetras.add(origem.strip())
+                vetorLetras.add(destino.strip())
+                print(f"origem: {origem} destino: {destino} peso: {peso}")
+
+        return len(vetorLetras)
 
     def buscaVizinhos(self, vertice):
         return self.grafo[vertice]
